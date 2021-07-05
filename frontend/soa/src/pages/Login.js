6c59b859a63c331c91c8b8604491650a17ffd7e8
Emanuel <e.alamilla318@gmail.com>
2021-07-05 00:57:48 -0500

    Cambios grandes

--- frontend/soa/src/pages/Login.js
+++ frontend/soa/src/pages/Login.js
@@ -4,28 +4,28 @@ export default class Login extends Component {
     render() {
         return (
             <form>
-                <h3>Sign In</h3>
+                <h3>Iniciar Sesion</h3>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Correo Electronico</label>
+                    <input type="email" className="form-control" placeholder="Ingrese correo electronico" />
                 </div>
 
                 <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <label>Contrasena</label>
+                    <input type="password" className="form-control" placeholder="Ingrese contrasena" />
                 </div>
 
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        <label className="custom-control-label" htmlFor="customCheck1">Recordarme</label>
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block">Iniciar Sesion</button>
                 <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
+                    Olvidaste la <a href="#">contrasena?</a>
                 </p>
             </form>
         );
--- frontend/soa/src/pages/SignUp.js
+++ frontend/soa/src/pages/SignUp.js
@@ -4,31 +4,84 @@ export default class SignUp extends Component {
     render() {
         return (
             <form>
-                <h3>Sign Up</h3>
+                <h3>Registrarse</h3>
 
                 <div className="form-group">
-                    <label>First name</label>
+                    <label>Nombre</label>
                     <input type="text" className="form-control" placeholder="First name" />
                 </div>
 
                 <div className="form-group">
-                    <label>Last name</label>
+                    <label>Apellidos</label>
                     <input type="text" className="form-control" placeholder="Last name" />
                 </div>
 
                 <div className="form-group">
-                    <label>Email address</label>
+                    <label>Correo Electronico</label>
                     <input type="email" className="form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
-                    <label>Password</label>
+                    <label>Contrasena</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <div className="form-group">
+                    <label>Fotografia del perfil</label>
+                    <input type="file" className="form-control"  />
+                </div>
+
+                <div className="form-group">
+                    <label>Acta de Nacimiento</label>
+                    <input type="file" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <label>INE/IFE</label>
+                    <input type="file" className="form-control" placeholder="Enter password" />
+                </div>
+                
+                <div className="form-group">
+                    <label>CURP</label>
+                    <input type="file" className="form-control" placeholder="Enter password" />
+                </div>
+                
+                <div className="form-group">
+                    <label>Comprobante de domicilio</label>
+                    <input type="file" className="form-control" placeholder="Enter password" />
+                </div>
+                
+                <div className="form-group">
+                    <label>Numero de tarjeta</label>
+                    <input type="text" className="form-control" placeholder="**** **** **** ****" />
+                </div>
+
+                <div className="form-group">
+                    <label>Fecha</label>
+                    <input type="text" className="form-control" placeholder="00/00" />
+                </div>
+
+                <div className="form-group">
+                    <label>CVV</label>
+                    <input type="text" className="form-control" placeholder="***" />
+                </div>
+                
+                
+                
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Acepta los terminos y condiciones?</label>
+                    </div>
+                </div>
+
+               
+               
+
+                <button type="submit" className="btn btn-primary btn-block">Registrarse</button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
+                    Ya estas registrado <a href="#">Inicia Sesion?</a>
                 </p>
             </form>
         );
