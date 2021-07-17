110c1fec5c7f28d4c24ce8aff22047019bf939e0
Changaloco <52005887+Changaloco@users.noreply.github.com>
2021-07-16 23:27:34 -0500

    sas
    sasas

--- frontend/soa/src/App.js
+++ frontend/soa/src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import AddressForm from './pages/Address';
+import  Checkout from './pages/Checkout';
+
 
 function App() {
   return (<Router>
@@ -35,6 +36,7 @@ function App() {
             <Route exact path='/' component={Login} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
+            <Route path="/checkout" component={Checkout} />
           </Switch>
         </div>
       </div>
