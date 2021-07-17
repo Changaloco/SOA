116eb7954e95c9849b9ce28ab744e1c3f7012788
FredyNaranjo19 <alfredonaranjo@micorreo.upp.edu.mx>
2021-07-16 23:11:26 -0500

    siu

--- frontend/soa/src/App.js
+++ frontend/soa/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import AddressForm from './pages/Address';
 
 function App() {
   return (<Router>
@@ -20,9 +19,6 @@ function App() {
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-up"}>Registrarse</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/Address"}>Pagos</Link>
               </li>
             </ul>
           </div>
